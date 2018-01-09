fc7535d15876fc3d40bae28e8d9d5cad627c4d6f
bruceSz <zsyuyizhang@gmail.com>
2018-01-09 23:42:06 +0800

    first commit

---
+++ exercise/classify_comment.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+import random
+import pickle
+from collections import Counter
+
+pos_txt = "../../data/tf_dat/pos.txt"
+neg_txt = "../../data/tf_dat/neg.txt"
+
+def create_lexicon(pos_file,neg_file):
+    lex = []
+    def process_file(f):
+        with open(pos_file,'r') as f:
+            lex = []
+            lines = f.readlines()
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
+
+
